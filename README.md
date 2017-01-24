f41cc5516646a21ee243bcbe741b771bc30cfd8c
Jan Tourlamain <jan.tourlamain@tobania.be>
2017-01-24 22:21:28 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,15 @@
+# AuthDemo
+
+
+## What is this?
+A Xamarin demo application that shows how to use OAuth without Xamarin.Auth.
+
+## Contributors
+
+- Jan Tourlamain
+
+## More info
+[http://www.devprotocol.com](http://www.devprotocol.com)
+
+## License
+Under MIT (See LICENSE.md)
---
+++ src/DevProtocol.Xam.Demos.AuthDemo/Configuration/ApiKeys.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DevProtocol.Xam.Demos.AuthDemo.Configuration
+{
+	public static class ApiKeys
+	{
+	public const string ClientId = "APP-ID-FROM-VSTS";
+	public const string ClientSecret = "APP-SECRET-FROM-VSTS";
+	public const string RedirectUrl = "YOUR-REDIRECT-URL";
+	public const string Scope = "vso.identity vso.profile";
+	public const string AuthorizeUrl = "https://app.vssps.visualstudio.com/oauth2/authorize";
+	public const string AccessTokenUrl = "https://app.vssps.visualstudio.com/oauth2/token";
+	public const string BaseAuthorizationUrl = "https://app.vssps.visualstudio.com/oauth2";
+	public const string ApiVersion = "2.0";
+	public const string AccessToken = "access_token";
+	}
+}
