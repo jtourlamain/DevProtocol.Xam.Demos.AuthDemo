a96e2de4cfc2d8f7d69fa7e1845f80091f7b4538
Jan Tourlamain <jan.tourlamain@tobania.be>
2017-01-25 00:11:07 +0100

    Added some bindings

--- src/DevProtocol.Xam.Demos.AuthDemo/Pages/LoginPage.xaml.cs
+++ src/DevProtocol.Xam.Demos.AuthDemo/Pages/LoginPage.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using DevProtocol.Xam.Demos.AuthDemo.ViewModels;
 using Xamarin.Forms;
 
 namespace DevProtocol.Xam.Demos.AuthDemo.Pages
@@ -9,7 +9,30 @@ namespace DevProtocol.Xam.Demos.AuthDemo.Pages
 	{
 		public LoginPage()
 		{
+			BindingContext = new LoginViewModel();
 			InitializeComponent();
+		}
+
+		private LoginViewModel ViewModel
+		{
+			get { return BindingContext as LoginViewModel; }
+		}
+
+		protected override void OnAppearing()
+		{
+			base.OnAppearing();
+			webAuth.Navigated += WebAuth_Navigated;
+		}
+
+		void WebAuth_Navigated(object sender, WebNavigatedEventArgs e)
+		{
+			ViewModel.VerifyLoginCommand.Execute(e.Url);
+		}
+
+		protected override void OnDisappearing()
+		{
+			base.OnDisappearing();
+			webAuth.Navigated -= WebAuth_Navigated;
 		}
 	}
 }
--- src/DevProtocol.Xam.Demos.AuthDemo/Pages/MainPage.xaml.cs
+++ src/DevProtocol.Xam.Demos.AuthDemo/Pages/MainPage.xaml.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using DevProtocol.Xam.Demos.AuthDemo.Configuration;
+using DevProtocol.Xam.Demos.AuthDemo.ViewModels;
 using Xamarin.Forms;
 
-namespace DevProtocol.Xam.Demos.AuthDemo
+namespace DevProtocol.Xam.Demos.AuthDemo.Pages
 {
 	public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
+			BindingContext = new MainViewModel();
 			InitializeComponent();
+		}
+
+		private MainViewModel ViewModel
+		{
+			get { return BindingContext as MainViewModel; }
+		}
+
+		protected override void OnAppearing()
+		{
+			MessagingCenter.Subscribe<MainViewModel>(this, MessageKeys.NavigateToLogin, async _ =>
+			  {
+				  await Navigation.PushModalAsync(new LoginPage());
+			  });
+			MessagingCenter.Subscribe<MainViewModel>(this, MessageKeys.NavigateToRepos, async _ =>
+			  {
+				  await Navigation.PushAsync(new ReposPage());
+			  });
+			base.OnAppearing();
+		}
+
+		protected override void OnDisappearing()
+		{
+			MessagingCenter.Unsubscribe<MainViewModel>(this, MessageKeys.NavigateToLogin);
+			MessagingCenter.Unsubscribe<MainViewModel>(this, MessageKeys.NavigateToRepos);
+			base.OnDisappearing();
 		}
 	}
 }
